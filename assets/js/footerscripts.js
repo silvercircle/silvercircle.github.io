bc73b7cb0627182a609ec9cd45a44a9d957b6c98
Alex <silvercircle@gmail.com>
2017-09-16 05:21:27 +0200

    share buttons

--- assets/js/footerscripts.js
+++ assets/js/footerscripts.js
@@ -407,6 +407,31 @@ function onMenuArrowClick(el)
 	return(false);
 }
 
+/*
+ * center a popup window on screen. 
+ * Credit: https://stackoverflow.com/questions/4068373/center-a-popup-window-on-screen
+ *
+ * original function: http://www.xtf.dk/2011/08/center-new-popup-window-even-on.html
+ */
+
+function PopupCenter(url, title, w, h) {
+    // Fixes dual-screen position                         Most browsers      Firefox
+    var dualScreenLeft = window.screenLeft != undefined ? window.screenLeft : screen.left;
+    var dualScreenTop = window.screenTop != undefined ? window.screenTop : screen.top;
+
+    var width = window.innerWidth ? window.innerWidth : document.documentElement.clientWidth ? document.documentElement.clientWidth : screen.width;
+    var height = window.innerHeight ? window.innerHeight : document.documentElement.clientHeight ? document.documentElement.clientHeight : screen.height;
+
+    var left = ((width / 2) - (w / 2)) + dualScreenLeft;
+    var top = ((height / 2) - (h / 2)) + dualScreenTop;
+    var newWindow = window.open(url, title, 'scrollbars=yes, width=' + w + ', height=' + h + ', top=' + top + ', left=' + left);
+
+    // Puts focus on the newWindow
+    if (window.focus) {
+        newWindow.focus();
+    }
+}
+
 $(document).ready(function(){
     // this kills the pure CSS hover effect from the dropdown menus so they will
     // only react on DOM events.
@@ -455,5 +480,10 @@ $(document).ready(function(){
             $(this).show();
             $("[data-target='" + $(this).attr('id') + "']").hide();
         }
+    });
+
+    $('.shareButtons-button').on('click', function(event) {
+        PopupCenter($(this).attr('href'), $(this).attr('data-title'), 640, 480);
+        return(false);
     });
 });
